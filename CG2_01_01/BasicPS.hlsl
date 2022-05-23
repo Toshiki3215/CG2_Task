23803e03bda029007134586cab0169c688ae4696
HatouToshiki <k021g1448@g.neec.ac.jp>
2022-05-23 17:37:31 +0900

    CG2_04_02_途中

--- CG2_01_01/BasicPS.hlsl
+++ CG2_01_01/BasicPS.hlsl
@@ -1,9 +1,11 @@
-cbuffer ConstBufferDataMaterial:register(b0)
+#include "Basic.hlsli"
+
+float4 main(VSOutput input) :SV_TARGET
 {
-	float4 color;  //êFÅ@(RGBA)
+	return float4(input.uv,0,1);
 }
 
-float4 main(float4 pos : SV_POSITION) : SV_TARGET
-{
-	return color;
-}
+//float4 main(float4 pos : SV_POSITION) : SV_TARGET
+//{
+//	return color;
+//}
--- CG2_01_01/main.cpp
+++ CG2_01_01/main.cpp
@@ -196,24 +196,52 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 	// --- 描画初期化処理　ここから --- //
 
-	//頂点データ
-	XMFLOAT3 vertices[] =
+	//頂点データ構造体
+	struct Vertex
 	{
-		//三角形なので３つ分の座標(x,y,z)を指定する。2Dの場合、zは0固定
-		{-0.5f,-0.5f,0.0f},//左下 インデックス0
-		{-0.5f,+0.5f,0.0f},//左上 インデックス1
-		{+0.5f,-0.5f,0.0f},//右下 インデックス2
-		{+0.5f,+0.5f,0.0f},//右上 インデックス3
+		XMFLOAT3 pos; //x,y,z座標
+		XMFLOAT2 uv;  //u,v座標
 	};
 
-	uint16_t indices[] =
+	//頂点データ
+	Vertex vertices[] =
+	{
+		//  x     y     z      u    v
+		{{-0.4f,-0.7f, 0.0f},{0.0f,1.0f}},
+		{{-0.4f,+0.7f, 0.0f},{0.0f,0.0f}},
+		{{+0.4f,-0.7f, 0.0f},{1.0f,1.0f}},
+		{{+0.4f,+0.7f, 0.0f},{1.0f,0.0f}},
+		/*{{-0.4f,-0.4f, 0.0f},{0.0f,1.0f}},
+		{{-0.4f,+0.4f, 0.0f},{0.0f,0.0f}},
+		{{+0.4f,-0.4f, 0.0f},{1.0f,1.0f}},
+		{{+0.4f,+0.4f, 0.0f},{1.0f,0.0f}},*/
+	};
+
+	//インデックスデータ
+	unsigned short indices[] =
 	{
 		0,1,2, //三角形1つ目
 		1,2,3, //三角形2つ目
 	};
 
+	//XMFLOAT3 vertices[] =
+	//{
+	//	//三角形なので３つ分の座標(x,y,z)を指定する。2Dの場合、zは0固定
+	//	{-0.5f,-0.5f,0.0f},//左下 インデックス0
+	//	{-0.5f,+0.5f,0.0f},//左上 インデックス1
+	//	{+0.5f,-0.5f,0.0f},//右下 インデックス2
+	//	{+0.5f,+0.5f,0.0f},//右上 インデックス3
+	//};
+
+	//uint16_t indices[] =
+	//{
+	//	0,1,2, //三角形1つ目
+	//	1,2,3, //三角形2つ目
+	//};
+
 	//頂点データ全体のサイズ = 頂点データ一つ分のサイズ * 頂点データの要素数
-	UINT sizeVB = static_cast<UINT>(sizeof(XMFLOAT3) * _countof(vertices));
+	//UINT sizeVB = static_cast<UINT>(sizeof(XMFLOAT3) * _countof(vertices));
+	UINT sizeVB = static_cast<UINT>(sizeof(vertices[0]) * _countof(vertices));
 
 	// 頂点バッファの設定
 	D3D12_HEAP_PROPERTIES heapProp{}; // ヒープ設定
@@ -239,7 +267,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	assert(SUCCEEDED(result));
 
 	// GPU上のバッファに対応した仮想メモリ(メインメモリ上)を取得
-	XMFLOAT3* vertMap = nullptr;
+	/*XMFLOAT3* vertMap = nullptr;
+	result = vertBuff->Map(0, nullptr, (void**)&vertMap);
+	assert(SUCCEEDED(result));*/
+
+	Vertex* vertMap = nullptr;
 	result = vertBuff->Map(0, nullptr, (void**)&vertMap);
 	assert(SUCCEEDED(result));
 
@@ -262,7 +294,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	vbView.SizeInBytes = sizeVB;
 
 	// 頂点1つ分のデータサイズ
-	vbView.StrideInBytes = sizeof(XMFLOAT3);
+	//vbView.StrideInBytes = sizeof(XMFLOAT3);
+	vbView.StrideInBytes = sizeof(vertices[0]);
 
 	ID3DBlob* vsBlob = nullptr; // 頂点シェーダオブジェクト
 	ID3DBlob* psBlob = nullptr; // ピクセルシェーダオブジェクト
@@ -321,14 +354,24 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	}
 
 	// 頂点レイアウト
+
+	//D3D12_INPUT_ELEMENT_DESC inputLayout[] =
+	//{
+	//	{
+	//		"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0,
+	//		D3D12_APPEND_ALIGNED_ELEMENT,
+	//		D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0
+	//	}, // (1行で書いたほうが見やすい)
+	//};
+
 	D3D12_INPUT_ELEMENT_DESC inputLayout[] =
 	{
-		{
-			"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0,
-			D3D12_APPEND_ALIGNED_ELEMENT,
-			D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0
-		}, // (1行で書いたほうが見やすい)
+		//x,y,z座標
+		{"POSITION",0,DXGI_FORMAT_R32G32_FLOAT,0,D3D12_APPEND_ALIGNED_ELEMENT,D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,0},
+		//u,v座標
+		{"TEXCOORD",0,DXGI_FORMAT_R32G32_FLOAT,0,D3D12_APPEND_ALIGNED_ELEMENT,D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA,0},
 	};
+
 
 	//インデックスデータ全体のサイズ
 	UINT sizeIB = static_cast<UINT>(sizeof(uint16_t) * _countof(indices));
